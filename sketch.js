c0d0904e71b43b431b65f62433b3f659945e5e1b
Manasvi56 <78258428+Manasvi56@users.noreply.github.com>
2021-03-19 07:39:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,34 +39,25 @@ function draw(){
         moving.velocityX = -3;
     }
 
-    if(isTouching(moving, fixed4)){
-        music.play();
-        moving.shapeColor="yellow";
-        bounceOff(moving,fixed4);
+    if(block1.isTouching(ball) && ball.bounceOff(fixed1)){ ball.shapeColor = rgb(0,0,255); music.play(); }
 
 
-    }
-else if(isTouching(moving, fixed3)){
-    music.stop();
-    moving.shapeColor="blue";
-    bounceOff(moving,fixed3);
+    if(block1.isTouching(ball) && ball.bounceOff(fixed2)){ ball.shapeColor = rgb(0,0,255); music.play(); }
 
-    }
-
-    else if(isTouching(moving, fixed2)){
+    if(isTouching(moving, fixed2) &&   bounceOff(moving,fixed2))
+    {
         music.stop();
         moving.shapeColor="green";
-        bounceOff(moving,fixed2);
         moving.velocityX=0;
         moving.velocityY=0;
 
 
     }
 
-    else if(isTouching(moving, fixed1)){
+     if(isTouching(moving, fixed1)&&         bounceOff(moving,fixed1)
+     ){
         music.stop();
         moving.shapeColor="red";
-        bounceOff(moving,fixed1);
 
     }
 
@@ -77,41 +68,5 @@ else if(isTouching(moving, fixed3)){
 
     drawSprites();
 
-}
-    function isTouching(object1,object2){
 
-        if(object1.x-object2.x<object2.width/2+object1.width/2&&object2.x-object1.x<object1.width/2+object2.width/2)
-        return true;
-    }
-
-else{
-
-    return false;
-    }
-
-
-
-
-function bounceOff(object1, object2){
-
-    if(object1.x-object2.x<object2.width/2+object1.width/2&&object2.x-object1.x<object1.width/2+object2.width/2){
-        object1.velocityX=object1.velocityX*-1
-        object2.velocityX=object2.velocityX*-1
-
-    }
-    if(object1.y-object2.y<object2.height/2+object1.height/2&&object2.y-object1.y<object1.height/2+object2.height/2){
-        object1.velocityY=object1.velocityY*-1
-        object2.velocityY=object2.velocityY*-1
-
-
-
-}
-
-if(object1.x<0){
-    object1. velocityX =3;
-}
-
-else if(object2.x>1200){
-    object2.velocityX =-3;
-}
 }
